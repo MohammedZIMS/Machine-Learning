b6ec6fafbf92c278caf96e693cdc1944c90b1d91
Md. Zahidul Islam Mehedi <zimss75656@gmail.com>
2025-07-28 22:11:55 +0600

    Improve variable call

--- Python/BasicPython/FunctionAndVariable.py
+++ Python/BasicPython/FunctionAndVariable.py
@@ -2,6 +2,11 @@ print("hello, world")
 
 # Variable: A variable is just a container for a value within your own program.
 
-name="Md. Zahidul Islam Mehedi"
+#name="Md. Zahidul Islam Mehedi"
+name = input("What's your name? ")
 print("Hello,")
 print(name)
+
+print("Hello,", name)
+print("Hello,"+ name + "\nHow are you?")
+print(f"Hello {name}! \nHow are you?")
