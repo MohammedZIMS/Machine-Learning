4d9a8cef2b0ac3c67789a65e789c6648d24d9895
Md. Zahidul Islam Mehedi <zimss75656@gmail.com>
2025-07-28 22:44:39 +0600

    Improve Formatting String use strip and title

--- Python/BasicPython/FunctionAndVariable.py
+++ Python/BasicPython/FunctionAndVariable.py
@@ -2,21 +2,23 @@ print("hello, world")
 
 # Variable: A variable is just a container for a value within your own program.
 
-#name="Md. Zahidul Islam Mehedi"
+    #name="Md. Zahidul Islam Mehedi"
 
 # Ask the user for their name
-name = input("What's your name? ")
-# print("Hello, ", end="")
-# print(name)
+name = input("What's your name? ").strip().title()
+    # print("Hello, ", end="")
+    # print(name)
 
-print(f"Hello {name}! How are you?")
+    # print(f"Hello {name}! How are you?")
 
-# print("Hello,", name)
-# print("Hello,"+ name + "\nHow are you?")
+    # print("Hello,", name)
+    # print("Hello,"+ name + "\nHow are you?")
 # Remove whitespace from the str
-name = name.strip()
-print(f"Hello {name}! How are you?")
+    # name = name.strip()
+    # print(f"Hello {name}! How are you?")
 
 # Remove whitespace from the str and capitalize the first letter of each word
-name = name.strip().title()
+    # name = name.strip().title()
+    # print(f"Hello {name}! How are you?")
+
 print(f"Hello {name}! How are you?")
