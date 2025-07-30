c1c9ec35611b905929d2c3822685738758521981
Md. Zahidul Islam Mehedi <zimss75656@gmail.com>
2025-07-30 21:48:05 +0600

    Hello funtion improve3

--- Python/BasicPython/Def.py
+++ Python/BasicPython/Def.py
@@ -1,5 +1,11 @@
+def main(): 
+    name= input("What is your name?")
+    hello(name)
+
+    hello()
+
 def hello(to="world"):
-    print("Hello, ", to)
+    print("Hello,", to)
 
     # name = input("What your name?")
     # hello()
@@ -8,7 +14,9 @@ def hello(to="world"):
     # name = input("What your name?")
     # hello(name)
 
-name = input("What your name?")
-hello(name)
+    # name = input("What your name?")
+    # hello(name)
 
-hello()
+    # hello()
+
+main()
