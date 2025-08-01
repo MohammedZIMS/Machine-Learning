021a0a23f728da8de0268caa567a7d2be4a15165
Md. Zahidul Islam Mehedi <zimss75656@gmail.com>
2025-08-01 21:32:50 +0600

    or logic

--- Python/Conditionals/ifStatements.py
+++ Python/Conditionals/ifStatements.py
@@ -2,9 +2,14 @@
 x = int(input("What is x:"))
 y = int(input("What is y:"))
 
-if x < y:
-    print("x is less then y")
-elif x > y:
-    print("x is greater than y")
+    # if x < y:
+    #     print("x is less then y")
+    # elif x > y:
+    #     print("x is greater than y")
+    # else:
+    #     print("x is equal to y")
+
+if x < y or x > y:
+    print("x and y is not equal")
 else:
     print("x is equal to y")
