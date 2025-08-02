afa88750db4df2704476e93550c379ea5034cef9
Md. Zahidul Islam Mehedi <zimss75656@gmail.com>
2025-08-02 20:45:45 +0600

    tuple improve

--- Python/OOP/student.py
+++ Python/OOP/student.py
@@ -1,22 +1,33 @@
-# def main():
-#     name = get_name()
-#     houre = get_house()
-#     print(f"{name} from {houre}")
+    # def main():
+    #     name = get_name()
+    #     houre = get_house()
+    #     print(f"{name} from {houre}")
 
-# def get_name():
-#     return input("Name: ")
+    # def get_name():
+    #     return input("Name: ")
 
-# def get_house():
-#     return input("House: ")
+    # def get_house():
+    #     return input("House: ")
+
+    # def main():
+    #     name, house = get_student()
+    #     print(f"{name} from {house}")
+
+    # def get_student():
+    #     name= input("Name: ")
+    #     house= input("House: ")
+    #     return name, house
 
 def main():
-    name, house = get_student()
-    print(f"{name} from {house}")
+    student = get_student()
+    if student[0] == "ZIMS":
+        student[1] = "Tolla"
+    print(f"{student[0]} from {student[1]}")
 
 def get_student():
     name= input("Name: ")
     house= input("House: ")
-    return name, house
+    return [name, house]
 
 if __name__ == "__main__":
     main()
