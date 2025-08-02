b9e192506cc666dc99da9de9a31d56b2ff27558b
Md. Zahidul Islam Mehedi <zimss75656@gmail.com>
2025-08-02 20:36:36 +0600

    tuple

--- Python/OOP/student.py
+++ Python/OOP/student.py
@@ -1,13 +1,22 @@
+# def main():
+#     name = get_name()
+#     houre = get_house()
+#     print(f"{name} from {houre}")
+
+# def get_name():
+#     return input("Name: ")
+
+# def get_house():
+#     return input("House: ")
+
 def main():
-    name = get_name()
-    houre = get_house()
-    print(f"{name} from {houre}")
+    name, house = get_student()
+    print(f"{name} from {house}")
 
-def get_name():
-    return input("Name: ")
-
-def get_house():
-    return input("House: ")
+def get_student():
+    name= input("Name: ")
+    house= input("House: ")
+    return name, house
 
 if __name__ == "__main__":
     main()
