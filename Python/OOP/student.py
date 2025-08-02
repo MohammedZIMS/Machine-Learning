fd37d8227c425154a785f1de7c744c37cf14b25b
Md. Zahidul Islam Mehedi <zimss75656@gmail.com>
2025-08-02 21:05:16 +0600

    Dictionary batter

--- Python/OOP/student.py
+++ Python/OOP/student.py
@@ -34,13 +34,16 @@
 # Dictionary.
 def main():
     student = get_student()
+    if student["name"] == "ZIMS":
+        student["house"] = "Tolla"
     print(f"{student['name']} from {student['house']}")
 
+
 def get_student():
-    student = {}
-    student["name"] = input("Name: ")
-    student["house"] = input("House: ")
-    return student
+    name = input("Name: ")
+    house = input("House: ")
+    return {"name": name, "house": house}
+
 
 if __name__ == "__main__":
     main()
